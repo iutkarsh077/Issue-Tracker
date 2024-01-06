77e83ba49febb613e9fc0546caaa96af7052295f
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-01-06 18:31:52 +0530

    ok

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,11 +3,11 @@ import { Callout, TextField } from "@radix-ui/themes";
 import { Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
-import axios from 'axios'
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { useState } from "react";
-import Spinner from '../../components/Spinner'
+import Spinner from "../../components/Spinner";
 
 interface IssueForm {
   title: string;
@@ -15,45 +15,51 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
-    const [Error, setError] = useState("");
-    const [isSubmit, setIsSubmit] = useState(false);
-    const router = useRouter();
+  const [Error, setError] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+  // const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-   <div className="max-w-xl">
-    {Error && <Callout.Root color="red" className="mb-5 font-bold">
-        <Callout.Text>{Error}</Callout.Text>
-    </Callout.Root>
-    }
-     <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async(data) =>
-        await axios.post('/api/issue', data).then((res)=>{
-          setIsSubmit(true);
-            console.log(res);
-            if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
-              router.push('/issues');
-          }
-        }).catch((err)=>{
-          setIsSubmit(false);
-            console.log(err);
-            setError("An unexpected error occured.")
-        })
+    <div className="max-w-xl">
+      {Error && (
+        <Callout.Root color="red" className="mb-5 font-bold">
+          <Callout.Text>{Error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(
+          async (data) =>
+            await axios
+              .post("/api/issue", data)
+              .then((res) => {
+                setIsSubmit(true);
+                console.log(res);
+
+                // router.push("/issues");
+              })
+              .catch((err) => {
+                setIsSubmit(false);
+                console.log(err);
+                setError("An unexpected error occured.");
+              })
         )}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button disabled={isSubmit}>Submit New issue{isSubmit && <Spinner/>}</Button>
-    </form>
-   </div>
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button disabled={isSubmit}>
+          Submit New issue{isSubmit && <Spinner />}
+        </Button>
+      </form>
+    </div>
   );
 };
 
